95aa09f09668a4d77633d2e13872189857c9c950
vlad <student3.oxygen.it.school@gmail.com>
2017-12-05 00:59:21 +0200

    fix tags2

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,9 +4,10 @@ module PostsHelper
  	tags.each do |tag|
  		#index = tag.count.to_f / max.count * (classes.size-1)
  		color = rand(1..5)
+ 		classes = 'tag'
  		classes = classes + color.to_s
  		yield(tag, classes)
- 		classes = 'tag'
+ 		
  	end	
  end
 end
