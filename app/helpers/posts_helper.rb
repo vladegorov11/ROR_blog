77d15275dd05e2c25051c87067c3c25f7fadf35f
vlad <student3.oxygen.it.school@gmail.com>
2017-12-05 00:52:50 +0200

    fix tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
  def tag_cloud(tags, classes)
- 	max = tags.sort_by(&:count).last
+ 	
  	tags.each do |tag|
  		#index = tag.count.to_f / max.count * (classes.size-1)
  		color = rand(1..5)
